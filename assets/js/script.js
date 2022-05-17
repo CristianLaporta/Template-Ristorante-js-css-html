6a40cadac791eb9f38bb4c8baf9201266e9832b5
Ottoemmezzo <sebastiano.Lazzeri@gmail.com>
2022-05-17 10:57:20 +0200

    java aggiornato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 /*Sebastian*/
-let flag=1,status=0, cursor=1;
+var flag=1,status=0, cursor=1;
 function slide_drag(event)
 {
     if (window.screen.width>991){
@@ -48,25 +48,62 @@ function slide_drag(event)
                     }
                 }    
     }
-    else if(window.screen.width<767)
+    else if(window.screen.width<=991)
         {
-            if (flag<3){
+            let s_dot="slide_dot_";
+            console.log("risoluzione<767!"+cursor)
+            if(cursor==1){
+                document.getElementById(s_dot+cursor).style.color="red";
+                document.getElementById(s_dot+(cursor+1)).style.color="gray";
+                document.getElementById(s_dot+(cursor+2)).style.color="gray";
+                document.getElementById(s_dot+(cursor+3)).style.color="gray";
+                cursor=2;
+            }
+            else if(cursor==2)
+            {
+                document.getElementById(s_dot+cursor).style.color="red";
+                document.getElementById(s_dot+(cursor-1)).style.color="gray";
+                document.getElementById(s_dot+(cursor+1)).style.color="gray";
+                document.getElementById(s_dot+(cursor+2)).style.color="gray";
+                cursor=3;
+            
+            }
+            else if(cursor==3)
+            {
+                document.getElementById(s_dot+cursor).style.color="red";
+                document.getElementById(s_dot+(cursor-1)).style.color="gray";
+                document.getElementById(s_dot+(cursor-2)).style.color="gray";
+                document.getElementById(s_dot+(cursor+1)).style.color="gray";
+                cursor=4;
+            
+            }
+
+            else if(cursor==4)
+            {
+                document.getElementById(s_dot+cursor).style.color="red";
+                document.getElementById(s_dot+(cursor-1)).style.color="gray";
+                document.getElementById(s_dot+(cursor-2)).style.color="gray";
+                document.getElementById(s_dot+(cursor-3)).style.color="gray";
+                cursor=1;
+            
+            }
+
+            if (flag==1){
+                    
+                    document.getElementById("slide_img_1").src="assets/images/slider/slider-img-"+(flag+1)+".jpg";
+                    document.getElementById("slide_img_2").src="assets/images/slider/slider-img-"+(flag+2)+".jpg";
+                    
+                    flag++;
+                    
+                }
+            else if (flag==2){
                     let s_dot="slide_img_";
                     document.getElementById("slide_img_1").src="assets/images/slider/slider-img-"+(flag+1)+".jpg";
-                    document.getElementById("slide_img_1").src="assets/images/slider/slider-img-"+(flag+2)+".jpg";
-                    if(cursor <4){
-                        document.getElementById(s_dot+cursor).style.color="red";
-                        document.getElementById(s_dot+(cursor+1)).style.color="gray";
-                        document.getElementById(s_dot+(cursor+2)).style.color="gray";
-                        document.getElementById(s_dot+(cursor+3)).style.color="gray";
-                    }
-                    flag++;
-                    cursor++;
-                }
-            else{
-                flag=1;
-                if(cursor==4){cursor=1;}
+                    document.getElementById("slide_img_2").src="assets/images/slider/slider-img-"+(flag+2)+".jpg";
+                    flag=1;
             }
+            else if(flag==3){flag=1;}
+            
 
         }
         
